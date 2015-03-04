dd28f29b3904eef43badb03d83b554a23da270d9
Brandon Yue <byue@cs.ualberta.ca>
2015-03-04 15:49:46 -0700

    Cleaned up the program a lot

--- src/VSRPart4.java
+++ src/VSRPart4.java
@@ -1,232 +1,143 @@
 import lejos.utility.Matrix;
 
-public class VSRPart4
-{
+
+public class VSRPart4 {
 
 	private TrackerReader tracker;
 	private Robot robot;
-
-	private double maxLocalIterations = 100.;
+	
 	private double minError = 20;
-	private double gain = 1;
+	private double gain = 0.2; //0.1-0.5
+	private double alpha = 0.2;//0.1-0.5
 	private int deltaAngle = 20;
-	double error = Double.MAX_VALUE;
-
-	private double maxLocalPoints;
-	private double alpha;
-
+	private double error = Double.MAX_VALUE;
+	
 	private Matrix mdf = null;
-	public double x = 0;
-	public double y = 0;
-	public double targetX = 0;
-	public double targetY = 0;
-	public double midTargetX = 0;
-	public double midTargetY = 0;
-	public double baseX = 0;
-	public double baseY = 0;
-	public double obstX = 0;
-	public double obstY = 0;
-	// increase the performance...
-	double[] globalErrors = null;
-
-	public static void main(String[] args)
+	
+	public static void main(String [] args)
 	{
-
 		VSRPart4 vsr = new VSRPart4();
 		try
 		{
-			// Go to the midpoint between the base and the obstacle
-			vsr.getInitialPosition(true);
-			vsr.calculateJacobian(0, 0);
-			vsr.inverseNewtonWithInitialGuess(vsr.midTargetX, vsr.midTargetY);
-			vsr.inverseNewtonWithInitialGuess(vsr.targetX, vsr.targetY);
-			// vsr.inverseNewton(vsr.targetX,vsr.targetY,0,0);
-			
-		} catch (Exception e)
-		{
-			e.printStackTrace();
+			vsr.calculateJacobian(0,0);
+			vsr.inverseNewton();
 		}
+		catch(Exception e){e.printStackTrace();}
 		vsr.disconnect();
 	}
-
-	public VSRPart4()
-	{
-		try
-		{
-			robot = new Robot();
-		} catch (Exception e)
-		{
-			System.err
-					.println("Couldn't connect to the EV3: " + e.getMessage());
+	
+	
+	public VSRPart4(){
+		try{ robot = new Robot();
+		}catch(Exception e){
+			System.err.println("Couldn't connect to the EV3: " + e.getMessage());
 			return;
 		}
 		tracker = new TrackerReader();
-		tracker.launchService(); // locking, until it's tracking an object
+		tracker.launchService(); //locking, until it's tracking an object
 	}
-
-	public void disconnect()
-	{
-
+	
+	public void disconnect(){
 		robot.rotateToMotorA(0);
 		robot.rotateToMotorB(0);
 		robot.close();
 		tracker.close();
 	}
-
-	public void getInitialPosition(boolean intermediate)
-	{
-
-		System.out.println("Getting initial points");
-		
-		x = tracker.x;
-		y = tracker.y;
-
-		targetX = tracker.targetx;
-		targetY = tracker.targety;
-		baseX = tracker.basex;
-		baseY = tracker.basey;
-		obstX = tracker.obstx;
-		obstY = tracker.obsty;
-		
-		midTargetX = (obstX + baseX) / 2;
-		midTargetY = (obstY + baseY) / 2;
-		midTargetX = (obstX + midTargetX) / 2;
-		midTargetY = (obstY + midTargetY) / 2;
-		
-		System.out.printf("Target: %f, %f   Mid: %f, %f   Base: %f, %f   Obst: %f, %f\n",
-				targetX,
-				targetY,
-				midTargetX,
-				midTargetY,
-				baseX,
-				baseY,
-				obstX,
-				obstY);
-		
-		maxLocalPoints = (int) (Math.hypot(targetX - x, targetY - y) / 60) + 1;
-		alpha = 1 / (5 * maxLocalPoints);
-		System.err.println("Creating " + maxLocalPoints + " local targets");
-	}
-
-	public void calculateJacobian(double a1, double a2)
-	{
-
+	
+	
+	public void calculateJacobian(double a1, double a2){
 		System.err.println("Updating Jacobian");
-		double[] cor = forwarKin(a1, a2);
-		double[] cor_1 = forwarKin(a1 + deltaAngle, a2);
-		double[] cor_2 = forwarKin(a1, a2 + deltaAngle);
-		double[][] df = new double[][] {
-				{ cor_1[0] - cor[0], cor_2[0] - cor[0] },
-				{ cor_1[1] - cor[1], cor_2[1] - cor[1] }, };
+		double[] cor = forwarKin(a1,a2);
+		double[] cor_1 = forwarKin(a1+deltaAngle,a2);
+		double[] cor_2 = forwarKin(a1,a2+deltaAngle);
+		double[][] df = new double[][]
+				{
+				{cor_1[0]-cor[0],cor_2[0]-cor[0]},
+				{cor_1[1]-cor[1],cor_2[1]-cor[1]},
+				};
 		mdf = new Matrix(df);
-		robot.rotateToMotorA((int) a1);
-		robot.rotateToMotorB((int) a2);
-		System.out.println("[" + mdf.get(0, 0) + " " + mdf.get(0, 1));
-		System.out.println(mdf.get(1, 0) + " " + mdf.get(1, 1) + "]");
+		robot.rotateToMotorA((int)a1);
+		robot.rotateToMotorB((int)a2);
+		System.out.println("["+mdf.get(0, 0)+" "+mdf.get(0, 1));
+		System.out.println(mdf.get(1, 0)+" "+mdf.get(1, 1)+"]");
 	}
-
-	public void inverseNewtonWithInitialGuess(double posX, double posY)
-	{
-		double[] dA = { 0.0, 0.0 };
-		double difX = (posX - x) / maxLocalPoints;
-		double difY = (posY - y) / maxLocalPoints;
-
-		for (int n = 1; n < maxLocalPoints + 1; n++)
-		{
-			System.err.println("LOCAL POINT: " + n + " out of "
-					+ (int) maxLocalPoints);
-			dA = inverseNewton(difX * n + x, difY * n + y, dA[0], dA[1], n);
-			if (!tracker.isConnected) // tracker is disconnected or lost
-				break;
-		}
-		if (tracker.isConnected)
-			System.err.println("TARGET REACHED!!! with error: " + (int) error
-					+ " pixels, max error allowed: " + minError);
-		try
-		{
-			Thread.sleep(5000);
-		} catch (InterruptedException e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	// desired position (pixels), current angles.
-	public double[] inverseNewton(double posX, double posY, double a1,
-			double a2, int iteration)
-	{
-
-		double[][] f = new double[][] { { tracker.x }, { tracker.y } };
-		double[][] dx = new double[][] { { a1 }, { a2 } };
-		double[][] dy = new double[][] { { 0 }, { 0 } };
-		double[][] y = new double[][] { { posX }, { posY } };
-		Matrix mdx = new Matrix(dx); // dQ
-		Matrix mdy = new Matrix(dy); // dS
+	
+	public void inverseNewton() throws InterruptedException{
+		double a1,a2;
+		
+		double[][] f= new double[][]{{tracker.x},{tracker.y}};
+		double[][] dx = new double[][]{{0},{0}};
+		double[][] dy = new double[][]{{0},{0}};
+		double[][] y = new double[][]{{tracker.targetx},{tracker.targety}};
+		Matrix mdx = new Matrix(dx); //dQ
+		Matrix mdy = new Matrix(dy); //dS
 		Matrix my = new Matrix(y);
-		double er = -1;
 		error = Double.MAX_VALUE;
-		int n = 1;
-		while (error > minError)
-		{
-			double oldX = f[0][0];
-			double oldY = f[1][0];
-
+		
+		while(error>minError){
+			double dist = Math.hypot(tracker.x-tracker.targetx, tracker.y-tracker.targety);
+			gain = 1.-(dist/500);
+			if(gain>1)
+				gain=1;
+			else if(gain<0.1)
+				gain=0.1;
+			
+			y = new double[][]{{tracker.targetx},{tracker.targety}};
+			my = new Matrix(y);
+			
+			double oldX=f[0][0];
+			double oldY=f[1][0];
+			
 			Matrix mf = new Matrix(f);
 			Matrix minv = mdf.inverse();
-			Matrix merr = my.minus(mf); // this is error
+			Matrix merr = my.minus(mf); //this is error
 			Matrix minverr = minv.times(merr);
 			mdx = minverr.times(gain);
-
-			a1 = mdx.get(0, 0);
-			a2 = mdx.get(1, 0);
-			robot.rotateMotorA((int) a1);
-			robot.rotateMotorB((int) a2);
-			try
-			{
-				Thread.sleep(100);
-			} catch (InterruptedException e1)
-			{
-				e1.printStackTrace();
+			a1=mdx.get(0, 0);
+			a2=mdx.get(1, 0);
+			
+			double angle = Math.hypot(a1,a2);
+			if(angle>10){
+				mdx = minverr.times(10/angle);
+				a1=mdx.get(0, 0);
+				a2=mdx.get(1, 0);
 			}
-			f = new double[][] { { tracker.x }, { tracker.y } };
-			dy = new double[][] { { f[0][0] - oldX }, { f[1][0] - oldY } };
+				
+			robot.rotateMotorA((int)a1);
+			robot.rotateMotorB((int)a2);
+			Thread.sleep(100);
+			f = new double[][]{{tracker.x},{tracker.y}};
+			dy = new double[][]{{f[0][0]-oldX},{f[1][0]-oldY}};
 			mdy = new Matrix(dy);
-
-			Matrix mJacTimedQ = mdf.times(mdx);
-			Matrix mdS_Minus_JdQ = mdy.minus(mJacTimedQ);
+			
+			Matrix mJacTimedQ=mdf.times(mdx);
+			Matrix mdS_Minus_JdQ=mdy.minus(mJacTimedQ);
 			Matrix mTransDX = mdx.transpose();
 			Matrix mNumerator = mdS_Minus_JdQ.times(mTransDX);
 			Matrix mDenominator = mTransDX.times(mdx);
-			Matrix mDivision = mNumerator.times(1 / mDenominator.get(0, 0));
+			Matrix mDivision = mNumerator.times(1/mDenominator.get(0, 0));
 			Matrix mdJac = mDivision.times(alpha);
-			mdf.plusEquals(mdJac); // here update the jacobian....
-
-			error = Math.hypot(tracker.x - posX, tracker.y - posY);
-			System.out.println("Error " + error);
-			System.out.println("[" + mdf.get(0, 0) + " " + mdf.get(0, 1));
-			System.out.println(mdf.get(1, 0) + " " + mdf.get(1, 1) + "]");
-			if (n > maxLocalIterations)
-				break;
-			if (!tracker.isConnected) // tracker is disconnected or lost
+			mdf.plusEquals(mdJac); //here update the jacobian....
+			
+			error = Math.hypot(tracker.x-tracker.targetx, tracker.y-tracker.targety);
+			System.out.println("Error "+ error);
+			System.out.println("["+mdf.get(0, 0)+" "+mdf.get(0, 1));
+			System.out.println(mdf.get(1, 0)+" "+mdf.get(1, 1)+"]");
+			if(!tracker.isConnected) //tracker is disconnected or lost
 				break;
 		}
-
-		a1 = mdx.get(0, 0);
-		a2 = mdx.get(1, 0);
-		return new double[] { a1, a2, er };
+		
+		if(tracker.isConnected)
+			System.err.println("TARGET REACHED!!! with error: "+(int)error+" pixels, max error allowed: "+minError);
+		Thread.sleep(5000);
+		
 	}
-
-	public double[] forwarKin(double a1, double a2)
-	{
-
-		robot.rotateToMotorA((int) a1);
-		robot.rotateToMotorB((int) a2);
-		try
-		{
-			Thread.sleep(100);
-		} catch (InterruptedException e)
-		{
-		}
-		return new double[] { tracker.x, tracker.y };
+	
+	public double[] forwarKin(double a1, double a2) {
+		robot.rotateToMotorA((int)a1);
+		robot.rotateToMotorB((int)a2);
+		try {Thread.sleep(100);
+		} catch (InterruptedException e) {}
+		return new double[]{tracker.x,tracker.y};
 	}
 }
--- src/VisualServoRobot.java
+++ src/VisualServoRobot.java
@@ -6,32 +6,21 @@ public class VisualServoRobot {
 	private TrackerReader tracker;
 	private Robot robot;
 	
-	private double maxLocalIterations = 100.;
 	private double minError = 20;
 	private double gain = 0.2; //0.1-0.5
-	private int deltaAngle = 20;
-	double error = Double.MAX_VALUE;
-	
-	private double maxLocalPoints = 1;
 	private double alpha = 0.2;//0.1-0.5
+	private int deltaAngle = 20;
+	private double error = Double.MAX_VALUE;
 	
 	private Matrix mdf = null;
-//	public double x = 0;
-//	public double y = 0;
-//	public double targetX = 0;
-//	public double targetY = 0;
-	//increase the performance...
-	double[] globalErrors = null;
 	
 	public static void main(String [] args)
 	{
 		VisualServoRobot vsr = new VisualServoRobot();
 		try
 		{
-			vsr.getInitialPosition();
 			vsr.calculateJacobian(0,0);
-			vsr.inverseNewtonWithInitialGuess();
-			//vsr.inverseNewton(vsr.targetX,vsr.targetY,0,0);
+			vsr.inverseNewton();
 		}
 		catch(Exception e){e.printStackTrace();}
 		vsr.disconnect();
@@ -56,19 +45,6 @@ public class VisualServoRobot {
 	}
 	
 	
-	public void getInitialPosition()
-	{
-		System.out.println("Getting initial points");
-//		x=tracker.x;
-//		y=tracker.y;
-//		targetX=tracker.targetx;
-//		targetY=tracker.targety;
-		//maxLocalPoints = (int)(Math.hypot(targetX-x, targetY-y)/30) + 1;
-		//alpha = 1/(5*maxLocalPoints);
-		System.err.println("Creating "+maxLocalPoints+" local targets");
-	}
-	
-	
 	public void calculateJacobian(double a1, double a2){
 		System.err.println("Updating Jacobian");
 		double[] cor = forwarKin(a1,a2);
@@ -86,29 +62,8 @@ public class VisualServoRobot {
 		System.out.println(mdf.get(1, 0)+" "+mdf.get(1, 1)+"]");
 	}
 	
-	public void inverseNewtonWithInitialGuess(){
-		double[] dA = {0.,0.};
-		double difX=(tracker.targetx-tracker.x)/maxLocalPoints;
-		double difY=(tracker.targety-tracker.y)/maxLocalPoints;
-		
-		for (int n=1;n<maxLocalPoints+1;n++){
-			System.err.println("LOCAL POINT: "+n+" out of "+(int)maxLocalPoints);
-			dA=inverseNewton();
-			if(!tracker.isConnected) //tracker is disconnected or lost
-				break;
-		}
-		if(tracker.isConnected)
-			System.err.println("TARGET REACHED!!! with error: "+(int)error+" pixels, max error allowed: "+minError);
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	// desired position (pixels), current angles.
-	public double[] inverseNewton(){
-		double a1,a2,a3;
+	public void inverseNewton() throws InterruptedException{
+		double a1,a2;
 		
 		double[][] f= new double[][]{{tracker.x},{tracker.y}};
 		double[][] dx = new double[][]{{0},{0}};
@@ -117,19 +72,16 @@ public class VisualServoRobot {
 		Matrix mdx = new Matrix(dx); //dQ
 		Matrix mdy = new Matrix(dy); //dS
 		Matrix my = new Matrix(y);
-		double er=-1;
 		error = Double.MAX_VALUE;
-		int n=1;
+		
 		while(error>minError){
 			double dist = Math.hypot(tracker.x-tracker.targetx, tracker.y-tracker.targety);
-			
 			gain = 1.-(dist/500);
 			if(gain>1)
 				gain=1;
 			else if(gain<0.1)
 				gain=0.1;
-			//alpha = gain;
-			System.out.println("Alpha:"+alpha);
+			
 			y = new double[][]{{tracker.targetx},{tracker.targety}};
 			my = new Matrix(y);
 			
@@ -140,8 +92,6 @@ public class VisualServoRobot {
 			Matrix minv = mdf.inverse();
 			Matrix merr = my.minus(mf); //this is error
 			Matrix minverr = minv.times(merr);
-			
-			
 			mdx = minverr.times(gain);
 			a1=mdx.get(0, 0);
 			a2=mdx.get(1, 0);
@@ -153,16 +103,9 @@ public class VisualServoRobot {
 				a2=mdx.get(1, 0);
 			}
 				
-			System.out.println("Angle:" +angle);
 			robot.rotateMotorA((int)a1);
 			robot.rotateMotorB((int)a2);
-			try
-			{
-				Thread.sleep(100);
-			} catch (InterruptedException e1)
-			{
-				e1.printStackTrace();
-			}
+			Thread.sleep(100);
 			f = new double[][]{{tracker.x},{tracker.y}};
 			dy = new double[][]{{f[0][0]-oldX},{f[1][0]-oldY}};
 			mdy = new Matrix(dy);
@@ -180,15 +123,14 @@ public class VisualServoRobot {
 			System.out.println("Error "+ error);
 			System.out.println("["+mdf.get(0, 0)+" "+mdf.get(0, 1));
 			System.out.println(mdf.get(1, 0)+" "+mdf.get(1, 1)+"]");
-			if(n>maxLocalIterations)
-				break;
 			if(!tracker.isConnected) //tracker is disconnected or lost
 				break;
 		}
 		
-		a1 = mdx.get(0, 0);
-		a2 = mdx.get(1, 0);
-		return new double[]{a1,a2,er};
+		if(tracker.isConnected)
+			System.err.println("TARGET REACHED!!! with error: "+(int)error+" pixels, max error allowed: "+minError);
+		Thread.sleep(5000);
+		
 	}
 	
 	public double[] forwarKin(double a1, double a2) {
